4a18b3de5562f15b3a9bebbc8d7a76aae4f08bcb
Yuriy Paliy <Yuriy.Paliy@4finance.com>
2020-08-31 14:26:23 +0200

    monaco editor fix

--- src/components/config_form.jsx
+++ src/components/config_form.jsx
@@ -41,63 +41,7 @@ class ConfigForm extends React.Component {
 
   render() {
     const options = {
-      "acceptSuggestionOnCommitCharacter": true,
-      "acceptSuggestionOnEnter": "on",
-      "accessibilitySupport": "auto",
-      "autoIndent": false,
-      "validate": true,
-      "automaticLayout": true,
-      "codeLens": true,
-      "colorDecorators": true,
-      "contextmenu": true,
-      "cursorBlinking": "blink",
-      "cursorSmoothCaretAnimation": false,
-      "cursorStyle": "line",
-      "disableLayerHinting": false,
-      "disableMonospaceOptimizations": false,
-      "dragAndDrop": false,
-      "fixedOverflowWidgets": false,
-      "folding": true,
-      "foldingStrategy": "auto",
-      "fontLigatures": false,
-      "formatOnPaste": false,
-      "formatOnType": false,
-      "hideCursorInOverviewRuler": false,
-      "highlightActiveIndentGuide": true,
-      "links": true,
-      "mouseWheelZoom": false,
-      "multiCursorMergeOverlapping": true,
-      "multiCursorModifier": "alt",
-      "overviewRulerBorder": true,
-      "overviewRulerLanes": 2,
-      "quickSuggestions": true,
-      "quickSuggestionsDelay": 100,
-      "readOnly": false,
-      "renderControlCharacters": false,
-      "renderFinalNewline": true,
-      "renderIndentGuides": true,
-      "renderLineHighlight": "all",
-      "renderWhitespace": "none",
-      "revealHorizontalRightPadding": 30,
-      "roundedSelection": true,
-      "rulers": [],
-      "scrollBeyondLastColumn": 5,
-      "scrollBeyondLastLine": true,
-      "selectOnLineNumbers": true,
-      "selectionClipboard": true,
-      "selectionHighlight": true,
-      "showFoldingControls": "mouseover",
-      "smoothScrolling": false,
-      "suggestOnTriggerCharacters": true,
-      "wordBasedSuggestions": true,
-      "wordSeparators": "~!@#$%^&*()-=+[{]}|;:'\",.<>/?",
-      "wordWrap": "off",
-      "wordWrapBreakAfterCharacters": "\t})]?|&,;",
-      "wordWrapBreakBeforeCharacters": "{([+",
-      "wordWrapBreakObtrusiveCharacters": ".",
-      "wordWrapColumn": 80,
-      "wordWrapMinified": true,
-      "wrappingIndent": "none"
+      theme: 'vs-dark',
     }
 
     const {error} = this.state
@@ -112,7 +56,6 @@ class ConfigForm extends React.Component {
                 <MonacoEditor
                   onChange={this.handleChangeSсhemaInput}
                   language="json"
-                  theme="vs-dark"
                   width="500"
                   height="400"
                   options={options}
@@ -123,7 +66,6 @@ class ConfigForm extends React.Component {
                 <MonacoEditor
                   onChange={this.handleChangeUiSсhemaInput}
                   language="json"
-                  theme="vs-dark"
                   width="500"
                   height="400"
                   options={options}
